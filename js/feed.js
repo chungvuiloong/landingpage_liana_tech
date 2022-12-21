5bbf6da6e58f7d5f052cde257aabd1ca50dce992
Jere <jirimicvl@outlook.com>
2022-12-21 10:33:42 +0200

    fixed news feed with images. Added more liana g

--- js/feed.js
+++ js/feed.js
@@ -4,7 +4,6 @@ const feed_list = document.querySelector('#feed_list');
   feednami.load(url)
   .then(feed => {
     feed_list.value = ''
-    console.log(feed);
     for(let entry of feed.entries){
         let div = document.createElement('div');
         const time = entry.date;
@@ -17,6 +16,11 @@ const feed_list = document.querySelector('#feed_list');
         `
         <div class="col">
             <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+                <div>
+                    ${entry.enclosures.length === 0 ? "" : 
+                    `<img src="${entry.enclosures[0].url}" class="img-fluid" alt="logo" style='height: 100%; width: 100%; object-fit: contain'></img>`
+                    }
+                </div>
                 <div class="card-body">
                     <h6 class="card-subtitle my-2 mx-3 text-muted fs-6">${date}.${month}.${year}</h6>
                     <a href="${entry.link}" class="nav-link"><div class="fs-4 text-dark" style="line-height: 1.2; text-decoration: none;">${entry.title}</div></a>
@@ -28,3 +32,13 @@ const feed_list = document.querySelector('#feed_list');
         feed_list.appendChild(div);
     }
   });
+
+// <div class="col">
+//   <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+//       <div class="card-body">
+//           <h6 class="card-subtitle my-2 mx-3 text-muted fs-6">${date}.${month}.${year}</h6>
+//           <a href="${entry.link}" class="nav-link"><div class="fs-4 text-dark" style="line-height: 1.2; text-decoration: none;">${entry.title}</div></a>
+//           <p class="my-2 mx-3 card-text fs-5s " style="line-height: 1.2;">${(entry.summary === null ? "" : entry.summary)}</p>
+//       </div>
+//   </div>
+// </div>
